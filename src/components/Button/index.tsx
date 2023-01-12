8ff92ae06dead398f9e9a0a10da3dc70b9f969de
Alex <alexsandrojr.303@gmail.com>
2023-01-11 22:03:11 -0300

    feat: add components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,34 @@
+import React, { useContext, useMemo } from 'react';
+import { ThemeContext } from 'styled-components';
+import { ButtonProps } from './types';
+
+import { ContainerButton, Title, Loading } from './styles';
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  color = 'error',
+  mode = 'contained',
+  loading = false,
+  onPress,
+  ...rest
+}: ButtonProps) => {
+  const { colors } = useContext(ThemeContext);
+
+  const colorByMode = useMemo(() => {
+    return mode === 'outline' ? colors[color].main : colors[color].onMain;
+  }, [colors, color, mode]);
+
+  return (
+    <ContainerButton
+      color={colors[color].main}
+      borderColor={colors[color].main}
+      mode={mode}
+      onPress={onPress}
+      {...rest}>
+      <Title color={colorByMode}>{children}</Title>
+      {loading && <Loading size={15} color={colorByMode} />}
+    </ContainerButton>
+  );
+};
+
+export default Button;
